ba65616bb30ccbe3044ae5f56fbcb76b7527ecb2
Min Seop Kim <k_ms1998@naver.com>
2022-07-21 00:20:14 +0900

    Test #2: Test EmailAuthService.sendAuthCode()

---
+++ src/test/java/com/proejct/ClassActionClaim/service/server/EmailAuthServiceTest.java
@@ -0,0 +1,27 @@
+package com.proejct.ClassActionClaim.service.server;
+
+import com.proejct.ClassActionClaim.service.server.EmailAuthService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.mail.MessagingException;
+
+@SpringBootTest
+class EmailAuthServiceTest {
+
+    @Autowired
+    private EmailAuthService emailAuthService;
+
+    @Test
+    void givenEmail_whenSendingAuthCode_thenSuccess() throws MessagingException {
+        // Given
+        String email = "kms1998@sju.ac.kr";
+        // When
+        emailAuthService.sendAuthCode(email);
+
+        // Then
+
+    }
+
+}
