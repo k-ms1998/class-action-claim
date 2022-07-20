ee3f114499d2b837ff37e02c833d615bcb407baf
Min Seop Kim <k_ms1998@naver.com>
2022-07-20 17:25:06 +0900

    Add #1: Create UserResponse

---
+++ src/main/java/com/proejct/ClassActionClaim/dto/ResponseBody/UserResponse.java
@@ -0,0 +1,17 @@
+package com.proejct.ClassActionClaim.dto.ResponseBody;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserResponse<T> {
+
+    public String message;
+    public T data;
+
+    public UserResponse(String message) {
+        this.message = message;
+    }
+}
