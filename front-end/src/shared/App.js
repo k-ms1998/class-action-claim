e6671cffd16092059abe98923dfc1ab2d5c320b9
pip-izony <eeodqql09@gmail.com>
2022-07-24 18:31:17 +0900

    first react

---
+++ front-end/src/shared/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { Home, About } from '../pages';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>
+                    홈
+                </h2>
+            </div>
+        );
+    }
+}
+
+export default App;
