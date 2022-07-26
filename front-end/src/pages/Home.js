6d8009c9503285ea88215881a86a96c01df74df7
pip-izony <eeodqql09@gmail.com>
2022-07-26 21:42:14 +0900

    Fix #3: Recovered Rendering Error

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -3,10 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>
-                홈
-            </h2>
-        </div>
+            <h1>홈</h1>
+            <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트랍니다.</p>
+      </div>
     );
 };
 
