88f25ade1b1963bc25b2bf039f63350c7fa2dd2a
pip-izony <eeodqql09@gmail.com>
2022-07-28 20:58:47 +0900

    Update sign_in page

---
+++ front-end/src/pages/Sign_up.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sign_up = () => {
+    return (
+        <div>
+            <h1>회원가입</h1>
+            <p>Hello_world</p>
+      </div>
+    );
+};
+
+export default About;
