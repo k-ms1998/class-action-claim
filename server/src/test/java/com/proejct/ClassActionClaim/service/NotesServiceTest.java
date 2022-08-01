f5b597bb3a3249aa31d48048d215f2e5b45fea46
Min Seop Kim <k_ms1998@naver.com>
2022-08-01 17:12:47 +0900

    TDD(Red) #11: Test for Updating Notes

--- server/src/test/java/com/proejct/ClassActionClaim/service/NotesServiceTest.java
+++ server/src/test/java/com/proejct/ClassActionClaim/service/NotesServiceTest.java
@@ -101,4 +101,48 @@ class NotesServiceTest {
         Assertions.assertThat(data).hasSize(2);
     }
 
+    @Test
+    void givenNoteRequest_whenExecutingUpdateNote_thenSuccess() {
+        // Given
+        Student student = new Student("userA", "passwordA", "test@sju.ac.kr");
+        Student savedStudent = studentRepository.save(student);
+
+        Lecture lecture = new Lecture("001", "LecA", "ProfA");
+        Lecture savedLecture = lectureRepository.save(lecture);
+
+
+        String title = "Title #1";
+        String content = "Content #1";
+        Integer week = 1;
+        Long studentId = savedStudent.getId();
+        Long lectureId = savedLecture.getId();
+
+        Notes note = new Notes(title, content, week , savedLecture, savedStudent);
+        Notes savedNote = notesRepository.save(note);
+
+        String updatedTitle = "Title #2";
+        String updatedContent = "Content #2";
+        NotesRequestDTO updateNotesRequestDTO = new NotesRequestDTO(updatedTitle, updatedContent, week, studentId, lectureId);
+
+        // When
+        ToClientResponse<NotesResponseDTO> notesResponseDTOToClientResponse = notesService.updateNote(savedNote.getId(), updateNotesRequestDTO);
+
+        // Then
+        String message = notesResponseDTOToClientResponse.getMessage();
+        Assertions.assertThat(message).isEqualTo("Note Updated.");
+
+        Integer count = notesResponseDTOToClientResponse.getCount();
+        Assertions.assertThat(count).isEqualTo(1);
+
+        NotesResponseDTO data = notesResponseDTOToClientResponse.getData();
+        Assertions.assertThat(data.getTitle()).isEqualTo("Title #2");
+        Assertions.assertThat(data.getContent()).isEqualTo("Content #2");
+
+        /**
+         * Notes 를 새로 추가한게 아니라 기존에 있던 것을 업데이트 한것이기 때문에 하나의 Notes 만 저장되어 있어야됨
+         */
+        List<Notes> allNotes = notesRepository.findAll();
+        Assertions.assertThat(allNotes).hasSize(1);
+    }
+
 }
