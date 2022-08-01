8e0547b8640433f18dde910cb33ef8ea259afcbd
Min Seop Kim <k_ms1998@naver.com>
2022-08-01 16:26:54 +0900

    TDD(Green) #11: Logic for Reading Notes

--- server/src/main/java/com/proejct/ClassActionClaim/controller/NotesController.java
+++ server/src/main/java/com/proejct/ClassActionClaim/controller/NotesController.java
@@ -1,13 +1,14 @@
 package com.proejct.ClassActionClaim.controller;
 
+import com.proejct.ClassActionClaim.domain.Notes;
 import com.proejct.ClassActionClaim.dto.RequestBody.NotesRequestDTO;
 import com.proejct.ClassActionClaim.dto.ResponseBody.NotesResponseDTO;
+import com.proejct.ClassActionClaim.dto.ResponseBody.ToClientResponse;
 import com.proejct.ClassActionClaim.service.NotesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/notes")
@@ -16,8 +17,14 @@ public class NotesController {
 
     private final NotesService notesService;
 
-    @PostMapping
+    @GetMapping("/{week}")
+    public ToClientResponse<List<NotesResponseDTO>> getNotes(@PathVariable Integer week, @ModelAttribute NotesRequestDTO requestDTO) {
+        return notesService.getNotesByWeek(week, requestDTO);
+    }
+
+    @PostMapping("/save")
     public NotesResponseDTO saveNotes(@RequestBody NotesRequestDTO requestDTO) {
         return notesService.saveNote(requestDTO);
     }
+    
 }
