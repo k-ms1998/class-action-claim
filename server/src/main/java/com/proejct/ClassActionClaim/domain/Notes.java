35f7c4c7bcf1c3ddd1c0a4d5fbb1e277841a5def
Min Seop Kim <k_ms1998@naver.com>
2022-08-01 17:13:57 +0900

    TDD(Green) Edit #1: Edited Notes to enable updating Title and Content

--- server/src/main/java/com/proejct/ClassActionClaim/domain/Notes.java
+++ server/src/main/java/com/proejct/ClassActionClaim/domain/Notes.java
@@ -35,4 +35,12 @@ public class Notes extends IdDateTimeEntity {
         this.lecture = lecture;
         this.student = student;
     }
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
 }
