1781cd04147050c492955c83c9166c3d86d3f975
Min Seop Kim <k_ms1998@naver.com>
2023-01-08 23:47:36 +0900

    Add #18: 인증 링크 발송과 인증 링크 검증하기
    * EmailAuthController: 인증 링크 검증하는 api 추가
    * EmailLinkRequest: 인증 링크로부터 받아올 request body
    *EmailAuthService: 인증 링크 발송하고, 인증 링크를 검증하는 로직 구현

--- server/src/main/java/com/proejct/ClassActionClaim/controller/server/EmailAuthController.java
+++ server/src/main/java/com/proejct/ClassActionClaim/controller/server/EmailAuthController.java
@@ -1,21 +1,22 @@
 package com.proejct.ClassActionClaim.controller.server;
 
 import com.proejct.ClassActionClaim.dto.RequestBody.EmailAuthRequest;
+import com.proejct.ClassActionClaim.dto.RequestBody.EmailLinkRequest;
 import com.proejct.ClassActionClaim.dto.StudentRequestDTO;
 import com.proejct.ClassActionClaim.service.server.EmailAuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/mail")
 public class EmailAuthController {
 
     private final EmailAuthService emailAuthService;
@@ -23,14 +24,21 @@ public class EmailAuthController {
     /**
      * Email 전송시
      */
-    @PostMapping("/mail/send_auth_code")
+    @PostMapping("/send_auth_code")
     public void sendAuthCode(@RequestBody StudentRequestDTO studentRequestDTO, HttpServletRequest request, HttpServletResponse response) {
 
         emailAuthService.sendAuthCode(studentRequestDTO, request, response);
     }
 
-    @PostMapping("/mail/authenticate")
+    @PostMapping("/authenticate")
     public void authenticateCode(@RequestBody EmailAuthRequest emailAuthData, HttpServletRequest request, HttpServletResponse response) {
         emailAuthService.authenticateAuthCode(emailAuthData, request, response);
     }
+
+    @PostMapping("/authenticate-link")
+    public void authenticateLink(@RequestBody EmailLinkRequest emailLinkRequest) {
+        log.info("{EmailAuthController} authenticate-link");
+        emailAuthService.authenticateLink(emailLinkRequest);
+    }
+
 }
