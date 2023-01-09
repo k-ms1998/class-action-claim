8fa88d88d91c7d597725a4d6ba95258127db6439
Min Seop Kim <k_ms1998@naver.com>
2023-01-09 16:25:18 +0900

    front-end 파일 복구

--- front-end/src/pages/board/BoardHome.js
+++ front-end/src/pages/board/BoardHome.js
@@ -1,27 +1,5 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-<<<<<<< HEAD
-import { HashRouter } from 'react-router-dom';
-import Header from "./Header";
-import Body from "./Body";
-import Footer from "./Footer";
-import ReactDOM from 'react-dom';
-
-
-class BoardHome extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <Header/>
-          <Body/>
-        <Footer/>
-      </HashRouter>
-    );
-  }
-}
-
-export default BoardHome;
-=======
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
@@ -37,4 +15,3 @@ const BoardHome = () => {
 }
 
 export default BoardHome
->>>>>>> main
--- front-end/src/pages/board/Body.js
+++ front-end/src/pages/board/Body.js
@@ -4,11 +4,7 @@ import BoardForm from "./BoardForm";
 import BoardWriteForm from "./BoardWriteForm";
 import BoardDetail from "./BoardDetail";
 import MypageForm from "./MypageForm";
-<<<<<<< HEAD
-import { Route } from "react-router-dom";
-=======
 import { Route, Routes } from "react-router-dom";
->>>>>>> main
 import $ from "jquery";
 import {} from "jquery.cookie";
 
@@ -16,41 +12,23 @@ class Body extends Component {
   render() {
     let resultForm;
     function getResultForm() {
-<<<<<<< HEAD
-      console.log($.cookie("login_id"));
-      if ($.cookie("login_id")) {
-        resultForm = <Route exact path="/" component={BoardForm}></Route>;
-        return resultForm;
-      } else {
-        resultForm = <Route exact path="/" component={Sign_in}></Route>;
-=======
       //console.log($.cookie("login_id"));
       if ($.cookie("login_id")) {
         resultForm = <Route path="/" element={<BoardForm/>}/>;
         return resultForm;
       } else {
         resultForm = <Route path="/" element={<Sign_in/>}/>;
->>>>>>> main
         return resultForm;
       }
     }
     getResultForm();
     return (
-<<<<<<< HEAD
-      <div>
-        <Route path="/mypage" component={MypageForm}></Route>
-        <Route path="/boardWrite" component={BoardWriteForm}></Route>
-        <Route path="/board/detail" component={BoardDetail}></Route>
-        {resultForm}
-      </div>
-=======
         <Routes>
           {resultForm}
           <Route path="/mypage" element={<MypageForm/>}/>
           <Route path="/boardWrite" element={<BoardWriteForm/>}/>
           <Route path="/board/detail" element={<BoardDetail/>}/>
         </Routes>
->>>>>>> main
     );
   }
 }
