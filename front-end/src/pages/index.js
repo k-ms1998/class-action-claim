a8942e5310dacd549f6185561f9a27d15327e3bb
pip-izony <eeodqql09@gmail.com>
2023-01-17 13:16:14 +0900

    origin update

--- front-end/src/pages/index.js
+++ front-end/src/pages/index.js
@@ -1,13 +1,7 @@
 export { default as Home } from './Home';
 export { default as About } from './About';
-<<<<<<< HEAD
 export { default as BoardHome } from './BoardHome';
 export { default as BoardWriteForm } from './BoardWriteForm';
 export { default as BoardDetail } from './BoardDetail';
 export { default as BoardForm } from './BoardForm';
 export { default as Sign_up } from './Sign_up';
-=======
-export { default as BoardHome } from './board/BoardHome';
-export { default as Sign_up } from './Sign_up';
-export { default as Validation } from './Validation';
->>>>>>> 49139fd59d9fdbbcb31babc5db9b06b585ab5f59
--- front-end/src/shared/App.js
+++ front-end/src/shared/App.js
@@ -9,14 +9,10 @@ const App = (props) => {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
-<<<<<<< HEAD
         <Route path="/BoardHome" element={<BoardHome />}/>
         <Route path="/BoardWriteForm" element={<BoardWriteForm />}/>
         <Route path="/BoardDetail" element={<BoardDetail />}/>
         <Route path="/BoardForm" element={<BoardForm />}/>
-=======
-        <Route path="/board/BoardHome/*" element={<BoardHome />}/>
->>>>>>> 49139fd59d9fdbbcb31babc5db9b06b585ab5f59
         <Route path="/sign_up" element={<Sign_up />}/>
       </Routes>
       </div>
